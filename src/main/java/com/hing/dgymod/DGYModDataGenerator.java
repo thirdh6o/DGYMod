1869c710f0a46242002c74e1aba1718bc4e237c0
“thirdh6o” <2514340934@qq.com>
2025-02-11 16:54:51 +0800

    Initial commit

---
+++ src/main/java/com/hing/dgymod/DGYModDataGenerator.java
@@ -0,0 +1,11 @@
+package com.hing.dgymod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class DGYModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
