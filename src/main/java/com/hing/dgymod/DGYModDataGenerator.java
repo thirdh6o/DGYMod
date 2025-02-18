19747e241d71bae34ef1e9f787ccfbd94b1261a2
“thirdh6o” <2514340934@qq.com>
2025-02-18 12:42:17 +0800

    盔甲（未完全）

--- src/main/java/com/hing/dgymod/DGYModDataGenerator.java
+++ src/main/java/com/hing/dgymod/DGYModDataGenerator.java
@@ -1,11 +1,12 @@
 package com.hing.dgymod;
-
+import com.hing.dgymod.datagen.*;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
 public class DGYModDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
-
+		//FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
+		//pack.addProvider(ModModelsProvider::new);
 	}
 }
